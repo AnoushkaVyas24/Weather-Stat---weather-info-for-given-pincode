c5494ed3e558803b05f38e24b01c8ba6bdf4e683
Anoushka Vyas <anuyvas2405@gmail.com>
2025-08-30 00:59:39 +0530

    Project structure initiated

---
+++ src/test/java/com/Anoushka/Weather_info_for_pincode/TestWeatherInfoForPincodeApplication.java
@@ -0,0 +1,11 @@
+package com.Anoushka.Weather_info_for_pincode;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestWeatherInfoForPincodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(WeatherInfoForPincodeApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
---
+++ src/test/java/com/Anoushka/Weather_info_for_pincode/WeatherInfoForPincodeApplicationTests.java
@@ -0,0 +1,15 @@
+package com.Anoushka.Weather_info_for_pincode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class WeatherInfoForPincodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
